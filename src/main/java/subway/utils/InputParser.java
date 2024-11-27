cfbfca1b9efe682fdbe8e272cafce365d57a66db
Chaeruin <cms3604@naver.com>
2024-11-27 12:27:34 +0900

    feat: parser 포함 utils 구현 완료

--- src/main/java/subway/utils/InputParser.java
+++ src/main/java/subway/utils/InputParser.java
@@ -1,4 +1,34 @@
 package subway.utils;
 
+import subway.domain.StationLine;
+
 public class InputParser {
+
+    public String getMenuInput(String input) {
+        if (InputValidator.isValidMenuInput(input)) {
+            return input;
+        }
+        return null;
+    }
+
+    public String getPathInput(String input) {
+        if (InputValidator.isValidPathInput(input)) {
+            return input;
+        }
+        return null;
+    }
+
+    public StationLine getStartStation(String input) {
+        if (InputValidator.isExistStations(input)) {
+            return Finder.getFindStation(input);
+        }
+        return null;
+    }
+
+    public StationLine getEndStation(String start, String end) {
+        if (InputValidator.isSameStations(start, end) && InputValidator.isExistStations(end)) {
+            return Finder.getFindStation(end);
+        }
+        return null;
+    }
 }
