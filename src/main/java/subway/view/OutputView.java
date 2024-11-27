c1e5838d12a2ae26aa96d1e4d8828a667ccdc188
Chaeruin <cms3604@naver.com>
2024-11-27 12:09:36 +0900

    feat: outputView 결과 조회 틀 + 나머지 전체 설계 완료

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,4 +1,30 @@
 package subway.view;
 
 public class OutputView {
+
+    public void printEnter() {
+        System.out.println();
+    }
+
+    public void printMainMenu() {
+        System.out.println("## 메인 화면\n"
+                + "1. 경로 조회\n"
+                + "Q. 종료\n");
+    }
+
+    public void printPathCrit() {
+        System.out.println("## 경로 기준\n"
+                + "1. 최단 거리\n"
+                + "2. 최소 시간\n"
+                + "B. 돌아가기\n");
+    }
+
+    public void printResult() {
+        System.out.println("## 조회 결과\n"
+                + "[INFO] ---\n");
+        System.out.println("[INFO] 총 거리: " + "km");
+        System.out.println("[INFO] 총 소요 시간: " + "분");
+        System.out.println("[INFO] ---");
+        // 경유 역 프린트
+    }
 }
